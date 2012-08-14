f74b7ce68b7d9b70f87c6885be90689e7ed2235b
Tim Almdal <tnalmdal@shaw.ca>
2012-08-13 21:24:40 -0700

    Restruction development environment and move SLopes to its own repository.

--- CoalToDiamonds/src/main/java/com/timalmdal/bukkit/coal2diamond/Coal2DiamondPlugin.java
+++ CoalToDiamonds/src/main/java/com/timalmdal/bukkit/coal2diamond/Coal2DiamondPlugin.java
@@ -1,4 +1,23 @@
 /*
+ * This file is part of coal2diamonds.
+ *
+ * Copyright (c) 2012, Tim Almdal <http://www.timalmdal.com/CoalToDiamonds/>
+ * coal2diamonds is licensed under the GNU Lesser General Public License.
+ *
+ * coal2diamonds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * coal2diamonds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  *
  * Copyright (c) 2012, Tim Almdal <http://www.timalmdal.com/>
  * CoalToDiamonds plugin is licensed under the GNU Lesser General Public License.
